58785dff711d4f3dfd00c875e9e8be5d63e7fbeb
alkthg2-stack <alkthg2@gmail.com>
2026-04-07 14:00:49 +0200

    Mission 1 (Correction) :  Afficher les voitures

---
+++ src/Voiture.java
@@ -0,0 +1,14 @@
+public class Voiture {
+
+    String Marque;
+    int Modele;
+    int Annee;
+    int Prix;
+
+    public Voiture(String Marque,int Modele,int Annee, int Prix){
+        this.Marque=Marque;
+        this.Modele=Modele;
+        this.Annee=Annee;
+        this.Prix=Prix;
+    }
+}
