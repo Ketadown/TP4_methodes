1202e3e6acbef0fee675d4b4f8698a1ce12aaaba
alkthg2-stack <alkthg2@gmail.com>
2026-04-07 13:44:30 +0200

    Mission 1 :  Afficher les voitures

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    static void main() {
+        String Marque = "Peugeot";
+        int Modele = 208;
+        int Annee = 2021;
+        int Prix = 14500;
+
+        System.out.println("Marque : "+Marque);
+        System.out.println("Modèle : "+Modele);
+        System.out.println("Année : "+Annee);
+        System.out.println("Prix : "+Prix);
+    }
+}
